457c57e85b826c4c8fa041a057df8166185b96e6
drywitte <wittedanj@gmail.com>
2019-06-11 07:55:17 -0400

    moving computers

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,12 +1,15 @@
-const apiRoutes = require("./api");
 const db = require("../../models");
+const router = require("express").Router();
 
-module.exports = function(app) {
-  app.get("/api/users", function(req, res) {
-    db.User.findAll(
-    ).then(function(response) {
-        console.log("here's a response")
-      res.json(response);
-    });
-  });
-}
+router.route("/")
+  .get(function(app) {
+      console.log("hello")
+      db.User.findAll(
+      ).then(function(response) {
+          console.log("here's a response")
+        res.json(response);
+      })
+    })
+
+
+module.exports = router;
