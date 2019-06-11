db79d5725ab38bff74437317297f8a4f62622196
drywitte <dan@Dans-iMac.local>
2019-06-11 08:10:31 -0400

    got routes working

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,14 +2,13 @@ const db = require("../../models");
 const router = require("express").Router();
 
 router.route("/")
-  .get(function(app) {
-      console.log("hello")
+  .get(function(req, res) {
       db.User.findAll(
       ).then(function(response) {
-          console.log("here's a response")
-        res.json(response);
-      })
+        console.log("here's a response")
+      res.json(response);
     })
+  })
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,17 +8,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 // Add routes, both API and view
 app.use(routes);
 
-
-// // Start the API server
-// app.listen(PORT, function() {
-//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-// });
 
 db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
